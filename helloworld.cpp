3df71807c758a8bbf56b15e997d54cd03fa1516a
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-04 22:24:30 +0500

    Quick update

--- helloworld.cpp
+++ helloworld.cpp
@@ -140,40 +140,50 @@
 
 
 // Single Element in Sorted Array
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int singleelementinsortedarray(vector<int>& arr){
+//     int n = arr.size();
+//     if(n==1) return arr[0];
+// int start = 1;
+// int end = arr.size()-2;
+//     while (start <= end){
+//         int mid = start + (end-start)/2;
+//         if(mid == 0 && arr[0] != arr[1]) return arr[mid];
+//         if(mid == n-1 && arr[n-1] != arr[n-2]) return arr[mid];
+//         if(arr[mid-1] != arr[mid] && arr[mid] != arr[mid+1]) return arr[mid];
+//         if(mid % 2 == 0){
+//             if(arr[mid-1] == arr[mid]){
+//                 end = mid - 1;
+//             } else {
+//                 start = mid + 1;
+//             }
+//         } else {
+//             if(arr[mid-1] == arr[mid]){
+//                 start = mid + 1;
+//             } else {
+//                 end = mid - 1;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+// int main(){
+//     vector<int> arr = {1,1,2,3,3,4,4,5,5};
+//     int result = singleelementinsortedarray(arr);
+//     cout << result << endl;
+
+//  return 0;
+// }
+
+
+
+
 #include<iostream>
 #include<vector>
 using namespace std;
-int singleelementinsortedarray(vector<int>& arr){
-    int n = arr.size();
-    if(n==1) return arr[0];
-int start = 1;
-int end = arr.size()-2;
-    while (start <= end){
-        int mid = start + (end-start)/2;
-        if(mid == 0 && arr[0] != arr[1]) return arr[mid];
-        if(mid == n-1 && arr[n-1] != arr[n-2]) return arr[mid];
-        if(arr[mid-1] != arr[mid] && arr[mid] != arr[mid+1]) return arr[mid];
-        if(mid % 2 == 0){
-            if(arr[mid-1] == arr[mid]){
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        } else {
-            if(arr[mid-1] == arr[mid]){
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return -1;
-    }
-}
 int main(){
-    vector<int> arr = {1,1,2,3,3,4,4,5,5};
-    int result = singleelementinsortedarray(arr);
-    cout << result << endl;
-
  return 0;
 }
 
