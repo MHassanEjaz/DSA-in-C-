63e2cc3c471e80ad3551f918eba14ca65f8afd9e
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-05 23:36:43 +0500

    Quick update

--- helloworld.cpp
+++ helloworld.cpp
@@ -291,15 +291,52 @@
 
 
 // Aggressive Cows Problem
-// #include<iostream>
-// #include<vector>
-// using namespace std;
+#include<iostream>
+#include<vector>
+using namespace std;
+bool ispossible(vector<int>& arr, int n, int c, int minalloweddistance){
+    int cows = 1;
+    int laststallposition = 0;
+    for (int i=0;i<n;i++){
+        if (arr[i] - laststallposition >= minalloweddistance){
+            cows++;
+            laststallposition = arr[i];
+        } 
+        if(cows == c){
+            return true;
+        }
+
+        
+    }
+    return false;
+}
+
+int getdistance(vector<int>& arr, int n, int c){
+    sort(arr.start(), arr.end())
+    int start = 1;
+    int end = maxvalue - minvalue;
+    int ans = -1;
+    while(start <= end){
+        int mid = start + (end - start)/2;
+        if(ispossible(arr, n, c, mid)){  // left
+            ans = mid;
+            end = mid - 1;
+        } else {   // right
+            start = mid + 1;
+        }
+    }
+    return ans;
+
+}
 
 
-// int main(){
-   
-//  return 0;
-// }
+int main(){
+   int n = 5;
+   int c = 3;
+   vector<int> arr = {1,2,8,4,9};
+   cout << getdistance(arr, n, c) << endl;
+ return 0;
+}
 
 
 
