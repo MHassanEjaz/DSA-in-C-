e1e8b14b9bbb37e07d0090ad0841a107661e2b32
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-05 22:52:36 +0500

    Quick update

--- helloworld.cpp
+++ helloworld.cpp
@@ -235,16 +235,71 @@
 
 
 // Painters Partition Problem
-#include<iostream>
-#include<vector>
-using namespace std;
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// bool ispossible(vector<int>& arr, int n, int m, int maxallowedtime){
+//     int painters = 1;
+//     int time = 0;
+//     for (int i=0;i<n;i++){
+//         if (arr[i] > maxallowedtime) return false;
+//         if(time + arr[i] <= maxallowedtime){
+//             time += arr[i];
+//         } else {
+//             painters++;
+//             time = arr[i];
+//         }
+
+        
+//     }
+//     return painters <= m;
+// }
+// int mintimetopaint(vector<int>& arr, int n, int m){
+//     int sum = 0;
+//     int maxvalue = INT32_MIN;
+//     for(int i=0;i<n;i++){
+//         sum += arr[i];
+//         maxvalue = max(maxvalue, arr[i]);
+//     }
+//     int start = maxvalue;
+//     int end = sum;
+//     int ans = -1;
+//     while(start <= end){
+//         int mid = start + (end - start)/2;
+//         if(ispossible(arr, n, m, mid)){  // left
+//             ans = mid;
+//             end = mid - 1;
+//         } else {   // right
+//             start = mid + 1;
+//         }
+//     }
+//     return ans;
+// }
+
+
+// int main(){
+//     vector<int> arr = {40,30,10,20};
+//     int n = 4, m = 5;
+//     cout << mintimetopaint(arr, n, m);
+//  return 0;
+// }
 
 
 
-int main(){
-    
- return 0;
-}
+
+
+
+
+// Aggressive Cows Problem
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+
+// int main(){
+   
+//  return 0;
+// }
 
 
 
