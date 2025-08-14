18c81a6c86ad6a5828db559a7cfdbf260220c266
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-14 14:19:20 +0500

    Quick update

--- helloworld.cpp
+++ helloworld.cpp
@@ -1889,7 +1889,34 @@
 
 
 
-// Queue
+// // Queue
+// #include <iostream>
+// #include<queue>
+// using namespace std;
+
+// int main() {
+//     queue<int> q;
+//     q.push(11);
+//     q.push(15);
+//     q.push(13);
+//     cout << "Size of queue is: " << q.size() << endl;
+//     q.pop();
+//     q.pop();
+//     q.pop();
+//     cout << "Size of queue is: " << q.size() << endl;
+//     if(q.empty()){
+//      cout << "Queue is empty.\n";   
+//     } else {
+//         cout << "Queue is not empty.\n";
+//     }
+// }
+
+
+
+
+
+
+// Implement a Queue with class
 #include <iostream>
 #include<queue>
 using namespace std;
