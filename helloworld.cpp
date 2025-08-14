38408e4aeaa48f090f023b07529f6d55b1940249
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-14 15:40:51 +0500

    Quick update

--- helloworld.cpp
+++ helloworld.cpp
@@ -2274,25 +2274,68 @@
 
 
 // Reverse a queue
+// #include <iostream>
+// #include<queue>
+// #include<stack>
+// using namespace std;
+// queue<int> rev(queue<int> q){
+//     stack<int> s;
+//     while(!q.empty()){
+//         int element = q.front();
+//         q.pop();
+//         s.push(element);
+//     }
+//     while(!s.empty()){
+//         int element = s.top();
+//         s.pop();
+//         q.push(element);
+//     }
+//     return q;
+// }
+// int main() {
+//      queue<int> q;
+
+//     // Adding sample elements to queue
+//     q.push(1);
+//     q.push(2);
+//     q.push(3);
+//     q.push(4);
+//     q.push(5);
+
+//     cout << "Original Queue: ";
+//     queue<int> temp = q;  // To print original without modifying
+//     while (!temp.empty()) {
+//         cout << temp.front() << " ";
+//         temp.pop();
+//     }
+//     cout << endl;
+
+//     // Reverse the queue
+//     queue<int> reversed = rev(q);
+
+//     cout << "Reversed Queue: ";
+//     while (!reversed.empty()) {
+//         cout << reversed.front() << " ";
+//         reversed.pop();
+//     }
+//     cout << endl;
+//     return 0;
+// }
+
+
+
+
+
+// 
 #include <iostream>
 #include<queue>
 #include<stack>
 using namespace std;
 queue<int> rev(queue<int> q){
-    stack<int> s;
-    while(!q.empty()){
-        int element = q.front();
-        q.pop();
-        s.push(element);
-    }
-    while(!s.empty()){
-        int element = s.top();
-        s.pop();
-        q.push(element);
-    }
-    return q;
+    
 }
 int main() {
+     
     
     return 0;
 }
