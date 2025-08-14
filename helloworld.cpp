35c740279083b0691bf768ab5f6f5d9a18bf6af8
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-14 14:27:34 +0500

    Quick update

--- helloworld.cpp
+++ helloworld.cpp
@@ -1920,6 +1920,29 @@
 #include <iostream>
 #include<queue>
 using namespace std;
+class queue {
+    int *arr;
+    int front;
+    int rear;
+    int size;
+    public:
+    queue(){
+        size = 100001;
+    }
+
+
+    bool isempty() {
+
+    }
+
+    void enqueue(int data){
+
+    }
+
+    int dequeue() {
+        
+    }
+}
 
 int main() {
     queue<int> q;
