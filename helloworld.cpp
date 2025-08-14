9eeee9b591d476a734ace608732fb1124a7849a5
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-14 15:21:13 +0500

    Quick update

--- helloworld.cpp
+++ helloworld.cpp
@@ -2153,22 +2153,39 @@
 #include<iostream>
 #include<queue>
 using namespace std;
+class Deque {
+    int *arr;
+    int front;
+    int rear;
+    int size;
+    public:
+    Deque(int n){
+        size = n;
+        arr = new int[n];
+        front = -1;
+        rear = -1;
+
+    }
+
+    bool pushfront(int x){
+        if(front == 0 && rear == size -1 || rear == (front-1)%(size-1)){
+            return false;
+        }
+        else if(front == -1){
+            front = rear = 0;
+        }
+        else if(front == 0){
+            front = n-1;
+        } 
+        else {
+            front--;
+        }
+        arr[front] = x;
+        return true;
+        
+    }
+};
 int main(){
-deque<int> d;
-d.push_front(12);
-d.push_front(14);
-cout << d.front() << endl;
-cout << d.back() << endl;
-d.pop_front();
-cout << d.front() << endl;
-cout << d.back() << endl;
-d.pop_back();
-if(d.empty()){
-    cout << "Queue is empty.\n";
-}
-else {
-    cout <<"queue is not empty\n";
-}
 
     return 0;
 }
