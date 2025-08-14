ec6fc56591bc25dd3c1ecfa6b1df36a038a63708
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-14 15:38:45 +0500

    Quick update

--- helloworld.cpp
+++ helloworld.cpp
@@ -2149,123 +2149,151 @@
 
 
 
+// Doubly queue with class
+// #include <iostream>
+// using namespace std;
 
+// class Deque {
+//     int* arr;
+//     int front;
+//     int rear;
+//     int size;
+
+// public:
+//     Deque(int n) {
+//         size = n;
+//         arr = new int[n];
+//         front = -1;
+//         rear = -1;
+//     }
+
+//     bool isFull() {
+//         return (front == 0 && rear == size - 1) || ((rear + 1) % size == front);
+//     }
+
+//     bool isEmpty() {
+//         return front == -1;
+//     }
+
+//     bool pushFront(int x) {
+//         if (isFull()) return false;
+
+//         if (isEmpty()) {
+//             front = rear = 0;
+//         } else if (front == 0) {
+//             front = size - 1;
+//         } else {
+//             front--;
+//         }
+
+//         arr[front] = x;
+//         return true;
+//     }
+
+//     bool pushRear(int x) {
+//         if (isFull()) return false;
+
+//         if (isEmpty()) {
+//             front = rear = 0;
+//         } else if (rear == size - 1 && front != 0) {
+//             rear = 0;
+//         } else {
+//             rear++;
+//         }
+
+//         arr[rear] = x;
+//         return true;
+//     }
+
+//     int popFront() {
+//         if (isEmpty()) return -1;
+
+//         int value = arr[front];
+//         arr[front] = -1;
+
+//         if (front == rear) {
+//             front = rear = -1;
+//         } else if (front == size - 1) {
+//             front = 0;
+//         } else {
+//             front++;
+//         }
+
+//         return value;
+//     }
+
+//     int popRear() {
+//         if (isEmpty()) return -1;
+
+//         int value = arr[rear];
+//         arr[rear] = -1;
+
+//         if (front == rear) {
+//             front = rear = -1;
+//         } else if (rear == 0) {
+//             rear = size - 1;
+//         } else {
+//             rear--;
+//         }
+
+//         return value;
+//     }
+
+//     int getFront() {
+//         if (isEmpty()) return -1;
+//         return arr[front];
+//     }
+
+//     int getRear() {
+//         if (isEmpty()) return -1;
+//         return arr[rear];
+//     }
+// };
+// int main() {
+//     Deque dq(5);
+
+//     dq.pushRear(10);
+//     dq.pushRear(20);
+//     dq.pushFront(5);
+//     dq.pushFront(2);
+
+//     cout << "Front: " << dq.getFront() << endl;
+//     cout << "Rear: " << dq.getRear() << endl;
+
+//     dq.popRear();
+//     dq.popFront();
+
+//     cout << "After pops:" << endl;
+//     cout << "Front: " << dq.getFront() << endl;
+//     cout << "Rear: " << dq.getRear() << endl;
+
+//     return 0;
+// }
+
+
+
+
+// Reverse a queue
 #include <iostream>
+#include<queue>
+#include<stack>
 using namespace std;
-
-class Deque {
-    int* arr;
-    int front;
-    int rear;
-    int size;
-
-public:
-    Deque(int n) {
-        size = n;
-        arr = new int[n];
-        front = -1;
-        rear = -1;
+queue<int> rev(queue<int> q){
+    stack<int> s;
+    while(!q.empty()){
+        int element = q.front();
+        q.pop();
+        s.push(element);
     }
-
-    bool isFull() {
-        return (front == 0 && rear == size - 1) || ((rear + 1) % size == front);
+    while(!s.empty()){
+        int element = s.top();
+        s.pop();
+        q.push(element);
     }
-
-    bool isEmpty() {
-        return front == -1;
-    }
-
-    bool pushFront(int x) {
-        if (isFull()) return false;
-
-        if (isEmpty()) {
-            front = rear = 0;
-        } else if (front == 0) {
-            front = size - 1;
-        } else {
-            front--;
-        }
-
-        arr[front] = x;
-        return true;
-    }
-
-    bool pushRear(int x) {
-        if (isFull()) return false;
-
-        if (isEmpty()) {
-            front = rear = 0;
-        } else if (rear == size - 1 && front != 0) {
-            rear = 0;
-        } else {
-            rear++;
-        }
-
-        arr[rear] = x;
-        return true;
-    }
-
-    int popFront() {
-        if (isEmpty()) return -1;
-
-        int value = arr[front];
-        arr[front] = -1;
-
-        if (front == rear) {
-            front = rear = -1;
-        } else if (front == size - 1) {
-            front = 0;
-        } else {
-            front++;
-        }
-
-        return value;
-    }
-
-    int popRear() {
-        if (isEmpty()) return -1;
-
-        int value = arr[rear];
-        arr[rear] = -1;
-
-        if (front == rear) {
-            front = rear = -1;
-        } else if (rear == 0) {
-            rear = size - 1;
-        } else {
-            rear--;
-        }
-
-        return value;
-    }
-
-    int getFront() {
-        if (isEmpty()) return -1;
-        return arr[front];
-    }
-
-    int getRear() {
-        if (isEmpty()) return -1;
-        return arr[rear];
-    }
-};
+    return q;
+}
 int main() {
-    Deque dq(5);
-
-    dq.pushRear(10);
-    dq.pushRear(20);
-    dq.pushFront(5);
-    dq.pushFront(2);
-
-    cout << "Front: " << dq.getFront() << endl;
-    cout << "Rear: " << dq.getRear() << endl;
-
-    dq.popRear();
-    dq.popFront();
-
-    cout << "After pops:" << endl;
-    cout << "Front: " << dq.getFront() << endl;
-    cout << "Rear: " << dq.getRear() << endl;
-
+    
     return 0;
 }
+
