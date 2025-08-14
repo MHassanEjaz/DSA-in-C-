9867ac20c6ea43332ea7a3370a4e3045b7395b03
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-14 14:15:19 +0500

    Quick update

--- helloworld.cpp
+++ helloworld.cpp
@@ -1891,19 +1891,13 @@
 
 // Queue
 #include <iostream>
-
+#include<queue>
 using namespace std;
 
-class SpecialStack {
-private:
-    
-
-public:
-    
-
-
-};
-
 int main() {
-    
+    queue<int> q;
+    q.push(11);
+    q.push(15);
+    q.push(13);
+    cout << "Size of queue is: " << q.size() << endl;
 }
