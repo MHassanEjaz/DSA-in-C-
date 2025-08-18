469c24736237ec2f5363c864462682806e5164c0
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-18 21:36:40 +0500

    Quick update

--- helloworld.cpp
+++ helloworld.cpp
@@ -2733,6 +2733,64 @@
 
 
 
+// Circular queue
+#include<iostream>
+using namespace std;
+class circularqueue {
+int *arr;
+int currsize, cap;
+int f = 0, r = -1;
+public:
+circularqueue(int size){
+    cap = size;
+    arr = new int[cap];
+    f=0;
+    r=-1;
+}
+void push(int data) {
+if(currsize == cap){
+    cout << "CQ is empty.\n";
+    return;
+}
+r = (r+1)%cap;
+arr[r] = data;
+currsize++;
+}
+
+void pop() {
+if(currsize == cap){
+    cout << "CQ is empty.\n";
+    return;
+}
+f = (f+1)%cap;
+currsize--;
+}
+
+int front() {
+    if(currsize == cap){
+    cout << "CQ is empty.\n";
+    return -1;
+}
+return arr[f];
+
+}
+
+bool empty ()
+{
+    return currsize == 0;
+
+}
+};
+
+int main(){
+
+
+    return 0;
+}
+
+
+
+
 
 
 
