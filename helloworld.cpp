3f5b2240f0aae1b14cc3afe0800d2381e4f79f0f
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-18 21:55:31 +0500

    Quick update

--- helloworld.cpp
+++ helloworld.cpp
@@ -2817,7 +2817,8 @@ int main(){
     int matrix[4][3] = {{1,2,3,}, {4,5,6}, {7,8,9} , {10,11,12}}; // 2D array
     int rows = 4;
     int cols = 3;
-    
+
+    // cout << matrix[2][1];
 
     return 0;
 }
