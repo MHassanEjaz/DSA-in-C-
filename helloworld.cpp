204df3585a0f2210245da10d92b1ee104d45f23e
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-18 22:01:54 +0500

    Quick update

--- helloworld.cpp
+++ helloworld.cpp
@@ -2813,12 +2813,22 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[5] = {1,2,3,4,5}; // Single array
+    // int arr[5] = {1,2,3,4,5}; // Single array
     int matrix[4][3] = {{1,2,3,}, {4,5,6}, {7,8,9} , {10,11,12}}; // 2D array
+     int matrix[4][3]; // for input
     int rows = 4;
     int cols = 3;
 
     // cout << matrix[2][1];
+    //  for input
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            cin >> matrix[i][j];
+        }
+        cout << endl;
+    }
+
+    // output
     for(int i=0;i<rows;i++){
         for(int j=0;j<cols;j++){
             cout << matrix[i][j];
