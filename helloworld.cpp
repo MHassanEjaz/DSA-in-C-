9490c38482225567c0e8aee3ff86385d00b85652
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-18 22:38:23 +0500

    Quick update

--- helloworld.cpp
+++ helloworld.cpp
@@ -2876,25 +2876,56 @@
 
 
 // Maximum row sum
+// #include<iostream>
+// using namespace std;
+// int getrowmaxsum(int matrix[4][3], int rows, int cols){
+//     int maxsum = INT16_MIN;
+//     for(int i=0;i<rows;i++){
+//         int rowsum = 0;
+//         for(int j=0;j<cols;j++){
+//             rowsum += matrix[i][j];
+//         }
+//         maxsum = max(maxsum, rowsum); 
+//     }
+//     return maxsum;
+// }
+// int main(){
+    
+//     int matrix[3][3] = {{1,2,3,}, {4,5,6}, {7,8,9}}; // 2D array
+//     int rows = 3;
+//     int cols = 3;
+//     cout << getrowmaxsum(matrix, rows, cols);
+    
+    
+
+//     return 0;
+// }
+
+
+
+
+// Diagonal Sum
 #include<iostream>
 using namespace std;
-int getrowmaxsum(int matrix[4][3], int rows, int cols){
-    int maxsum = INT16_MIN;
-    for(int i=0;i<rows;i++){
-        int rowsum = 0;
-        for(int j=0;j<cols;j++){
-            rowsum += matrix[i][j];
+int diagonalsum(int matrix[4][4], int n){
+    int sum = 0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+           if(i == j){
+            sum += matrix[i][j];
+           }
+           else if(j = n-i-1){
+            sum += matrix[i][j];
+           }
         }
-        maxsum = max(maxsum, rowsum); 
     }
-    return maxsum;
+    return sum;
 }
 int main(){
     
-    int matrix[3][3] = {{1,2,3,}, {4,5,6}, {7,8,9}}; // 2D array
-    int rows = 3;
-    int cols = 3;
-    cout << getrowmaxsum(matrix, rows, cols);
+    int matrix[4][4] = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16}}; // 2D array
+    int n=4;
+    cout << diagonalsum(matrix, n);
     
     
 
