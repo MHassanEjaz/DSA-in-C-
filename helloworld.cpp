97f25a75cd849adcab52276e7f0e732d43b5ed48
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-25 22:20:08 +0500

    Quick update

--- helloworld.cpp
+++ helloworld.cpp
@@ -2905,29 +2905,42 @@
 
 
 // Diagonal Sum
+// #include<iostream>
+// using namespace std;
+// int diagonalsum(int matrix[4][4], int n){
+//     int sum = 0;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n;j++){
+//            if(i == j){
+//             sum += matrix[i][j];
+//            }
+//            else if(j = n-i-1){
+//             sum += matrix[i][j];
+//            }
+//         }
+//     }
+//     return sum;
+// }
+// int main(){
+    
+//     int matrix[4][4] = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16}}; // 2D array
+//     int n=4;
+//     cout << diagonalsum(matrix, n);
+    
+    
+
+//     return 0;
+// }
+
+
+
+
+
+// Linked List
 #include<iostream>
 using namespace std;
-int diagonalsum(int matrix[4][4], int n){
-    int sum = 0;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-           if(i == j){
-            sum += matrix[i][j];
-           }
-           else if(j = n-i-1){
-            sum += matrix[i][j];
-           }
-        }
-    }
-    return sum;
-}
-int main(){
-    
-    int matrix[4][4] = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16}}; // 2D array
-    int n=4;
-    cout << diagonalsum(matrix, n);
-    
-    
+int main() {
+
 
     return 0;
 }
