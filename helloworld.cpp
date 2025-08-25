528532516919ffc9bb7576a8bd6b733fdfd41666
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-25 22:36:27 +0500

    Quick update

--- helloworld.cpp
+++ helloworld.cpp
@@ -2961,11 +2961,20 @@ void push_front(int val) {
     Node* newnode = new Node(val);
     if(head == NULL){
         head = tail = newnode;
+        return;
+    }
+    else {
+        newnode->next = head;
+        head = newnode;
     }
 
 }
 };
 int main() {
+    List ll;
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
 
 
     return 0;
