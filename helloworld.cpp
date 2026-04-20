50424f89d44ab7e2c23a22d9c344c9a825db09a1
MHassanEjaz <hassanejaz2216@gmail.com>
2026-04-20 19:29:10 +0500

    Quick update

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,18 +1,18 @@
-#include<iostream>
-using namespace std;
-int main(){
+// #include<iostream>
+// using namespace std;
+// int main(){
 
-cout << "Hello World";
-cout << "Welcome to Git Hub";
-cout << "Let's Check";
-cout << "Add Some Further code";
+// cout << "Hello World";
+// cout << "Welcome to Git Hub";
+// cout << "Let's Check";
+// cout << "Add Some Further code";
 
-cout << "Hello World";
-cout << "Welcome to Git Hub";
-cout << "Let's Check";
-cout << "Add Some Further code";
-return 0;
-}
+// cout << "Hello World";
+// cout << "Welcome to Git Hub";
+// cout << "Let's Check";
+// cout << "Add Some Further code";
+// return 0;
+// }
 
 
 
